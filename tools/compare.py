fcf9103a39cc5482b311b7ba4a9ff3bc455091aa
liviaruegger <analiviaruegger@gmail.com>
2023-10-09 22:13:27 -0300

    feat: updating comparison logic to work with more users

--- tools/compare.py
+++ tools/compare.py
@@ -3,24 +3,26 @@ from film import get_film, parse_film_data
 from utils import save_to_csv
 
 
-def compare_watchlists(user1: str, user2: str):
-    watchlist1 = [
-        f["endpoint"]
-        for f in get_complete_watchlist_data(
-            username=user1, complete_data=False
+def compare_watchlists(users: list[str]) -> list:
+    watchlists = []
+
+    for user in users:
+        watchlists.append(
+            [
+                f["endpoint"]
+                for f in get_complete_watchlist_data(
+                    username=user, complete_data=False
+                )
+            ]
         )
-    ]
-    watchlist2 = [
-        f["endpoint"]
-        for f in get_complete_watchlist_data(
-            username=user2, complete_data=False
-        )
-    ]
+
+    films_in_common = set(watchlists[0])
+    for watchlist in watchlists[1:]:
+        films_in_common = films_in_common.intersection(set(watchlist))
+    print(f"\n --- found {len(films_in_common)} films in common ---")
 
     film_data = []
-    films_in_common = set(watchlist1).intersection(set(watchlist2))
-    print(f"\n --- found {len(films_in_common)} films in common ---")
-    
+
     print(f"\n === Collecting film details ===")
     for endpoint in films_in_common:
         film_data.append(parse_film_data(get_film(endpoint)))
@@ -29,18 +31,22 @@ def compare_watchlists(user1: str, user2: str):
 
 
 def save_comparison_to_csv(
-    user1: str, user2: str, sort_by_rating: bool = False
+    users: list[str], sort_by_rating: bool = False
 ) -> None:
-    data = compare_watchlists(user1, user2)
+    data = compare_watchlists(users)
 
     if sort_by_rating:
         data = sorted(data, key=lambda x: x["average_rating"], reverse=True)
 
-    save_to_csv(data, f"{user1}_and_{user2}.csv")
+    save_to_csv(data, f"{users[0]}_and_friends.csv")
 
 
 if __name__ == "__main__":
-    user1 = input("Enter user: ")
-    user2 = input("Enter another user: ")
+    users = []
 
-    save_comparison_to_csv(user1, user2, True)
+    _input = input("Enter username: ")
+    while _input != "q":
+        users.append(_input)
+        _input = input("Enter username (or 'q' when you're done): ")
+
+    save_comparison_to_csv(users, True)
